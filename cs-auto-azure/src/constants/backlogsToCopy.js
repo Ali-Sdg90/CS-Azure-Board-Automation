f9a92223e1d35d5ae102e62c9325ebd8554a5c41
Ali Sadeghi <ali909392@gmail.com>
2025-08-04 13:36:22 +0330

    Refactor OperationalBacklogs: reorder entries, correct IDs, and improve naming consistency.

--- cs-auto-azure/src/constants/backlogsToCopy.js
+++ cs-auto-azure/src/constants/backlogsToCopy.js
@@ -9,32 +9,40 @@ export const OperationalBacklogs = [
         id: 6335,
     },
     {
+        name: "[TEMPLATE] Hold Technical Assistant Sessions - Level 1 - [Sprint No.]",
+        id: 32874,
+    },
+    {
+        name: "[TEMPLATE] Hold Technical Assistant Sessions - Level 2 - [Sprint No.]",
+        id: 34118,
+    },
+    {
+        name: "[TEMPLATE] Hold Technical Assistant Sessions - Level 3 - [Sprint No.]",
+        id: 34122,
+    },
+    {
         name: "[TEMPLATE] Team A (Mon. 19:00-20:00) Operational Meeting Routine Tasks [Sprint No.]",
         id: 25159,
     },
     {
-        name: "[TEMPLATE] Interns Coordination  [Sprint No.]",
-        id: 20521,
+        name: "[TEMPLATE] Interns Coordination [Sprint No.]",
+        id: 33525,
     },
     {
         name: "[TEMPLATE] Communication/Coaching workshop [Sprint No.]: Mon (20:30-21:00) - [sessions no.]",
         id: 32648,
     },
     {
-        name: "[TEMPLATE] Communication/Coaching workshop [Sprint No.]: Tue (15:00-15:30) - [sessions no.]",
-        id: 26147,
-    },
-    {
         name: "[TEMPLATE] Communication/Coaching workshop  [Sprint No.]: Sat (15:00-15:30) - [sessions no.]",
         id: 23482,
     },
     {
-        name: "[TEMPLATE] Q&A Session for [Intro 1, Intro 2, Intro 3] - Tue - [Sprint No.]",
-        id: 30035,
+        name: "[TEMPLATE] Q&A Session for [Intro 1 - Interview] - Sun - [Sprint No.]",
+        id: 28403,
     },
     {
-        name: "[TEMPLATE] Q&A Session for [Intro 1, Intro 2, Intro 3] - Sun - [Sprint No.]",
-        id: 28403,
+        name: "[TEMPLATE] Q&A Session for [Intro 2 - Intro 3] - Tue - [Sprint No.]",
+        id: 30035,
     },
 ];
 
